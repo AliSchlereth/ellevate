cd17d2f493fd4676971825c5787499758642f1ea
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-03 20:56:55 -0700

    Add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'auth/google', as: 'login'
   get 'auth/:provider/callback', to: "sessions#create"
+  get 'logout', to: "sessions#destroy"
   root "welcome#index"
   resources :students, only: [:show]
 end
