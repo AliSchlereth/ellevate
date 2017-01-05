aea7fbca4049d4776d167cb8216eeabf0067d7b4
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-04 20:00:55 -0700

    update Student level from data type string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104211328) do
+ActiveRecord::Schema.define(version: 20170105025807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170104211328) do
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.string   "username"
-    t.string   "level"
+    t.integer  "level"
     t.string   "language"
     t.integer  "teacher_id"
     t.integer  "pass_img_id"
