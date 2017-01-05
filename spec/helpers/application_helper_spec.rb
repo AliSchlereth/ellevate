8d4312d1f0609783eb8744e835a7e349444a2814
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-04 21:45:29 -0700

    Attempt to add helper spec unsuccessful" "

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+xdescribe "ApplicationHelper" do
+  context "current_students" do
+    it "returns an array of students by level belonging to current user" do
+      teacher = create(:teacher)
+      student1, student2, student3, student4 = create_list(:student, 4, teacher: teacher)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(teacher)
+
+      students = ApplicationHelper.current_students
+
+      expect(students).to be_a(Array)
+      expect(students[0].level).to be < students[-1]
+    end
+  end
+end
