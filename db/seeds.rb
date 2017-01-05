c983bbdf0b1749131e48ab41d154856743d176c0
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-04 19:33:37 -0700

    Add pass images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PassImg.create(img: 'book.svg')
+PassImg.create(img: 'bell.svg')
+PassImg.create(img: 'bug.svg')
+PassImg.create(img: 'gift.svg')
+PassImg.create(img: 'heart.svg')
+PassImg.create(img: 'home.svg')
+PassImg.create(img: 'key.svg')
+PassImg.create(img: 'lock.svg')
+PassImg.create(img: 'primitive-square.svg')
