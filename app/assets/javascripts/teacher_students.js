2e037d8c6affade2ed859bda51abd983878976dd
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-05 13:07:13 -0700

    Update seed file to use glyphicons for PassImgs

--- app/assets/javascripts/teacher_students.js
+++ app/assets/javascripts/teacher_students.js
@@ -7,20 +7,18 @@ var onGetStudents = function(data) {
       '<div class="col-md-2">' + student.language + '</div>' +
       '<div class="col-md-2">' + student.level + '</div>' +
       '<div class="col-md-2">' + student.level + '</div>' +
-      '<div class="col-md-2">' + student.passimg.img + '</div>' +
-
-
-    )
+      '<div class="col-md-2">' + student.img + '</div>'
+    );
   }
 }
 
 var getAllStudents = function() {
   return $.ajax({
     method: 'GET',
-    url: 'api/v1/teachers/students'
+    url: '/api/v1/teachers/students'
   })
-  .done(onGetStudents)
-  .fail(onFail);
+  .done(onGetStudents);
+  // .fail(onFail);
 }
 
 getAllStudents();
