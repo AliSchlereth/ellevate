4547c8b5dc80f09a5d2f8a46f2ef108e6cd37f10
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-05 14:36:28 -0700

    Find student belonging to delete button with jQuery and JS

--- app/assets/javascripts/teacher_students.js
+++ app/assets/javascripts/teacher_students.js
@@ -2,14 +2,22 @@ var onGetStudents = function(data) {
   for (var i = 0; i < data.length; i++) {
     var student = data[i];
     $('#tch-dash-students').append(
+      '<div class="dash-student" id="student-' + student.id + '">' +
       '<div class="col-md-2">' + student.username + '</div>' +
       '<div class="col-md-2">' + student.name + '</div>' +
       '<div class="col-md-2">' + student.language + '</div>' +
       '<div class="col-md-2">' + student.level + '</div>' +
       '<div class="col-md-2">' + student.level + '</div>' +
-      '<div class="col-md-2"><span class="' + student.img + '" aria-hidden="true"></span></div>'
+      '<div class="col-md-2"><span class="' + student.img + '" aria-hidden="true"></span></div>' +
+      '<div class="btn btn-danger btn-xs dash-stu-delete">Delete</div>' +
+      '</div>'
     );
   }
+  deleteStudents();
+}
+
+var onFail = function(err) {
+  console.error(err);
 }
 
 var getAllStudents = function() {
@@ -17,12 +25,23 @@ var getAllStudents = function() {
     method: 'GET',
     url: '/api/v1/teachers/students'
   })
-  .done(onGetStudents);
-  // .fail(onFail);
+  .done(onGetStudents)
+  .fail(onFail);
+}
+
+var deleteStudentById = function() {
+  var student = $(this).parents('div.dash-student')[0];
+  var stuId = student.id.split("-")[1]
+  debugger;
+  // var stuId = $(this).parents('div.dash-student')[0].id.split("-")[1]
 }
 
 getAllStudents();
 
-$(document).ready(function(){
+// $(document).ready(function(){
+  var deleteStudents = function() {
+    $('.dash-stu-delete').on('click', deleteStudentById);
 
-})
+  }
+
+// })
