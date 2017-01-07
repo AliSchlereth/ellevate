85cdad6acbc27fa4344f1e561463c2343d8ebd27
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-07 13:14:04 -0700

    Begin build of update student inline on tch dashboard

--- app/assets/javascripts/teacher_students.js
+++ app/assets/javascripts/teacher_students.js
@@ -4,16 +4,17 @@ var onGetStudents = function(data) {
     var student = data[i];
     $('#tch-dash-students').append(
       '<div class="dash-student" id="student-' + student.id + '">' +
-      '<div class="col-md-2">' + student.username + '</div>' +
-      '<div class="col-md-2">' + student.name + '</div>' +
-      '<div class="col-md-2">' + student.language + '</div>' +
-      '<div class="col-md-2">' + student.level + '</div>' +
-      '<div class="col-md-2"><span class="' + student.img + '" aria-hidden="true"></span></div>' +
+      '<div class="col-md-2 stu-username">' + student.username + '</div>' +
+      '<div class="col-md-2 stu-name">' + student.name + '</div>' +
+      '<div class="col-md-2 stu-language">' + student.language + '</div>' +
+      '<div class="col-md-2 stu-level">' + student.level + '</div>' +
+      '<div class="col-md-2 stu-pass-img"><span class="' + student.img + '" aria-hidden="true"></span></div>' +
       '<div class="btn btn-danger btn-xs dash-stu-delete">Delete</div>' +
+      '<div class="btn btn-warning btn-xs dash-stu-update">Edit</div>' +
       '</div>'
     );
   }
-  deleteStudents();
+  manageStudents();
 }
 
 var onFail = function(err) {
@@ -41,11 +42,11 @@ var deleteStudentById = function() {
 }
 
 var createStudentById = function() {
-  var stuName = $('input.stu-name').val();
-  var stuUsername = $('input.stu-username').val();
-  var stulanguage = $('input.stu-language').val();
-  var stuLevel = $('select.stu-level').val();
-  var stuPassImg = $('select.stu-pass-img').val();
+  var stuName = $('input.new-stu-name').val();
+  var stuUsername = $('input.new-stu-username').val();
+  var stulanguage = $('input.new-stu-language').val();
+  var stuLevel = $('select.new-stu-level').val();
+  var stuPassImg = $('select.new-stu-pass-img').val();
   return $.ajax({
     method: 'POST',
     url: '/api/v1/teachers/students',
@@ -53,18 +54,52 @@ var createStudentById = function() {
   })
   .done(function() {
     $('input').val('');
-    $('select.stu-level').val('1');
-    $('select.stu-pass-img').val('');
+    $('select.new-stu-level').val('1');
+    $('select.new-stu-pass-img').val('');
     getAllStudents();
   })
   .fail(onFail);
 }
 
+var editStudentField = function(stuInput) {
+  var currentValue = stuInput.innerHTML;
+  var currentClass = stuInput.classList[0];
+  stuInput.outerHTML = '<input class="col-md-2' +
+    currentClass + '" value="' + currentValue + '"/>'
+}
+
+var updateStudentById = function() {
+  var student = $(this).parents('.dash-student');
+  var stuName = student.find('.stu-name')[0];
+  var stuLanguage = student.find('.stu-language')[0];
+  var stuUsername = student.find('.stu-username')[0];
+  var stuInfo = [stuName, stuLanguage, stuUsername]
+  for (var i = 0; i < stuInfo.length; i++) {
+    editStudentField(stuInfo[i]);
+  }
+  var stuLevel = student.find('.stu-level')[0];
+  var currentClass = stuLevel.classList[0];
+  var currentLevel = stuLevel.innerHTML;
+  if (currentLevel === '3') {
+    stuLevel.outerHTML = '<select> <option value="one">1</option> <option value="two">2</option> <option selected value="three">3</option> </select>';
+  } else if (currentLevel === '2') {
+    stuLevel.outerHTML = '<select> <option value="one">1</option> <option selected value="two">2</option> <option value="three">3</option> </select>';
+  } else {
+    stuLevel.outerHTML = '<select> <option selected value="one">1</option> <option value="two">2</option> <option value="three">3</option> </select>';
+  }
+  
+
+
+
+}
+
 getAllStudents();
 
-var deleteStudents = function() {
+var manageStudents = function() {
   $('.dash-stu-delete').on('click', deleteStudentById);
+  $('.dash-stu-update').on('click', updateStudentById);
 }
+
 
 $(document).ready(function(){
   // var createStudents = function() {
