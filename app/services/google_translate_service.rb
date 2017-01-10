237828e736ba1f9441193553f426ffb7f0c605e5
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-09 19:05:32 -0700

    Refactor GoogleTranslateService

--- app/services/google_translate_service.rb
+++ app/services/google_translate_service.rb
@@ -1,7 +1,15 @@
 class GoogleTranslateService
 
-  def self.translate(language_code, message)
-    response = Faraday.get("https://translation.googleapis.com/language/translate/v2?key=#{ENV["GOOGLE_API_KEY"]}&source=en&target=#{language_code}&q=#{message}")
+  def initialize
+    @conn = Faraday.new(url: "https://translation.googleapis.com/language/translate/v2") do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+  def translate(language_code, message)
+    # response = Faraday.get("https://translation.googleapis.com/language/translate/v2?key=#{ENV["GOOGLE_API_KEY"]}&source=en&target=#{language_code}&q=#{message}")
+    response = @conn.get("?key=#{ENV["GOOGLE_API_KEY"]}&source=en&target=#{language_code}&q=#{message}")
     translation = JSON.parse(response.body, symbolize_names: true)
   end
 
--- spec/services/google_translate_service_spec.rb
+++ spec/services/google_translate_service_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "Google Translate Service" do
   context "translate" do
     it "returns a translated phrase" do
-      translation = GoogleTranslateService.translate("de", "My name is Jeff.")
+      translation = GoogleTranslateService.new.translate("de", "My name is Jeff.")
 
       expect(translation[:data]).to eq({:translations=>[{:translatedText=>"Mein Name ist Jeff."}]})
     end
