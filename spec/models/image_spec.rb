5e9a52277f0f4ac0e9654786c391eed4a65c567d
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-10 11:12:05 -0700

    Add Image.process_animal_tag spec

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -9,4 +9,15 @@ describe "Image" do
       expect(image).to respond_to('animal')
     end
   end
+
+  context "process_animal_tag" do
+    it "returns a selected string from split tags string" do
+      image_data = {hits: [{tags: "one, two, three"}, {tags: "four, five, six"}]}
+
+      tag = Image.process_animal_tag(image_data, 1)
+
+      expect(tag).to be_a(String)
+      expect(tag).to eq("four")
+    end
+  end
 end
